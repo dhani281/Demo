57f9d402338fdeb4f18840bcaaae1512c619608e
dhani <dhanimol.sebastian@arteriatech.com>
2016-07-14 19:00:53 +0530

    first commit

---
+++ CodeTest/TableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.h
+//  CodeTest
+//
+//  Created by Arteria on 14/07/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@end
---
+++ CodeTest/TableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.m
+//  CodeTest
+//
+//  Created by Arteria on 14/07/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

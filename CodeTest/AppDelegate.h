dfd11fb59de047da4c380f36c73c7c0597264c05
dhani <dhanimol.sebastian@arteriatech.com>
2016-07-14 10:38:12 +0530

    Initial Commit

---
+++ CodeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeTest
+//
+//  Created by Arteria on 14/07/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeTest
+//
+//  Created by Arteria on 14/07/16.
+//  Copyright © 2016 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
